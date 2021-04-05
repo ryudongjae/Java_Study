079d42ce4ffe6b6336ddffd623b7d50501f456e3
ryudongjae <rdj10149@gmail.com>
2021-04-05 16:54:16 +0900

    2021.04.05 study

---
+++ src/study02/InformationHidingTest.java
@@ -0,0 +1,13 @@
+package study02;
+
+public class InformationHidingTest {
+    public static void main(String[] args) {
+        InformationHiding md = new InformationHiding();
+        md.setYear(2020);
+        md.setMonth(2);
+        md.setDay(23);
+
+        md.showBrithday();
+
+    }
+}
