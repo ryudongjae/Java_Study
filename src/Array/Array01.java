fe9afb55bce5231d45b68b1789536c31bd2d3a11
ryudongjae <rdj10149@gmail.com>
2021-04-09 00:14:40 +0900

    Example(array)

---
+++ src/Array/Array01.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Array01 {
+
+
+
+    public static void main(String[] args) {
+        int [] arr = new int[10];
+        int total = 0;
+
+        for(int i=0, num=1; i<arr.length; i++,num++){
+            arr[i] = num;
+        }
+        for(int i=0; i<arr.length; i++){
+            total +=arr[i];
+        }
+        System.out.println(total);
+    }
+}
