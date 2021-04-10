f7c475a311a350213b65300400815d363c3a8640
ryudongjae <rdj10149@gmail.com>
2021-04-10 14:37:39 +0900

    Commit

--- src/inheritance/example001/CustomerTest.java
+++ src/inheritance/example001/CustomerTest.java
@@ -4,11 +4,25 @@ public class CustomerTest {
     public static void main(String[] args) {
         Customer customerLee = new Customer(10000,"LEE");
         customerLee.bonusPoint = 1000;
-        System.out.println(customerLee.showCustomerInfo());
+//        System.out.println(customerLee.showCustomerInfo());
 
 
         VIPCustomer customerKim = new VIPCustomer(10001,"KIM");
         customerKim.bonusPoint = 10000;
-        System.out.println(customerKim.showCustomerInfo());
+//        System.out.println(customerKim.showCustomerInfo());
+
+        int priceLee = customerLee.calcPrice(10000);
+        int priceKim = customerKim.calcPrice(10000);
+
+        System.out.println(customerLee.showCustomerInfo() + "지불금액은 "+ priceLee + "입니다 ");
+        System.out.println(customerKim.showCustomerInfo() + "지불금액은 "+ priceKim + "입니다 ");
+
+
+        VIPCustomer vc = new VIPCustomer(10002,"Ha");
+        vc.bonusPoint =1000;
+
+        int pricevc = vc.calcPrice(10000);
+        System.out.println(vc.showCustomerInfo() + "지불금액은 "+ pricevc + "입니다 ");
+
     }
 }
--- src/inheritance/example001/VIPCustomer.java
+++ src/inheritance/example001/VIPCustomer.java
@@ -12,6 +12,11 @@ public class VIPCustomer extends Customer{
             bonusRatio = 0.05;
             salesRatio = 0.1;
         }
+        @Override
+        public int calcPrice(int price){
+            bonusPoint += price*bonusRatio;
+            return price -(int)(price* salesRatio);
+        }
 
         public int getAgentID(){
             return agentID;
