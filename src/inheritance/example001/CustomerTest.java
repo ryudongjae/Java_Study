8b34aece9c887fb911ccfabf44f2e38c46b05183
ryudongjae <rdj10149@gmail.com>
2021-04-13 15:02:13 +0900

    Test

--- src/inheritance/example001/CustomerTest.java
+++ src/inheritance/example001/CustomerTest.java
@@ -7,18 +7,23 @@ public class CustomerTest {
 //        System.out.println(customerLee.showCustomerInfo());
 
 
-        VIPCustomer customerKim = new VIPCustomer(10001,"KIM");
+        VIPCustomer customerKim = new VIPCustomer(10001,"KIM",1222);
         customerKim.bonusPoint = 10000;
 //        System.out.println(customerKim.showCustomerInfo());
 
+
+        GoldCustomer customerHa = new GoldCustomer(10002,"HA");
+        customerHa.bonusPoint = 20000;
+
         int priceLee = customerLee.calcPrice(10000);
         int priceKim = customerKim.calcPrice(10000);
+        int priceHa = customerHa.calcPrice(10000);
 
         System.out.println(customerLee.showCustomerInfo() + "지불금액은 "+ priceLee + "입니다 ");
         System.out.println(customerKim.showCustomerInfo() + "지불금액은 "+ priceKim + "입니다 ");
+        System.out.println(customerHa.showCustomerInfo() + "지불금액은 "+ priceHa+ "입니다 ");
 
-
-        VIPCustomer vc = new VIPCustomer(10002,"Ha");
+        VIPCustomer vc = new VIPCustomer(10002,"PArk",1221);
         vc.bonusPoint =1000;
 
         int pricevc = vc.calcPrice(10000);
