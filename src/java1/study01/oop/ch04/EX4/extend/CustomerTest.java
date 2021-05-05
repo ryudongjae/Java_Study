3b2c8f5bc4724abe16d5f01a941f80566fecc253
ryudongjae <rdj10149@gmail.com>
2021-05-05 18:27:05 +0900

    extend.super()

---
+++ src/java1/study01/oop/ch04/EX4/extend/CustomerTest.java
@@ -0,0 +1,26 @@
+package java1.study01.oop.ch04.EX4.extend;
+
+import java1.inheritance.example001.VIPCustomer;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+
+        Customer customerLee = new Customer(10010, "이순신");
+        customerLee.bonusPoint = 1000;
+        System.out.println(customerLee.showCustomerInfo());
+
+        VIPCustomer customerKim = new VIPCustomer(10020, "김유신",12);
+        customerKim.bonusPoint = 10000;
+        System.out.println(customerKim.showCustomerInfo());
+
+        int priceLee = customerLee.calcPrice(10000);
+        int priceKim = customerKim.calcPrice(10000);
+
+        System.out.println(customerLee.showCustomerInfo() + " 지불금액은 " + priceLee + "원 입니다.");
+        System.out.println(customerKim.showCustomerInfo() + " 지불금액은 " + priceKim + "원 입니다.");
+
+        System.out.println(customerKim.showCustomerInfo());
+        System.out.println(customerLee.showCustomerInfo());
+
+    }
+}
