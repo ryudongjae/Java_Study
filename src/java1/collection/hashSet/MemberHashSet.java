f832c7310783d485391edf888c4a108b17a2d261
ryudongjae <rdj10149@gmail.com>
2021-05-11 17:16:43 +0900

    collection

--- src/java1/collection/hashSet/MemberHashSet.java
+++ src/java1/collection/hashSet/MemberHashSet.java
@@ -1,6 +1,7 @@
 package java1.collection.hashSet;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class MemberHashSet {
     private HashSet<Member> hashSet;
@@ -13,6 +14,27 @@ public class MemberHashSet {
         hashSet.add(member);
     }
 
+    public boolean removeMember(int memberId){
 
+        Iterator<Member> ir = hashSet.iterator();
+
+        while(ir.hasNext()){
+            Member member = ir.next();
+            int temp = member.getMemberId();
+            if(temp == memberId){
+                hashSet.remove(member);
+                return true;
+            }
+        }
+        System.out.println(memberId + "가 존ㄴ재하지 않습니다.");
+        return false;
+    }
+
+    public void showMember(){
+        for (Member member :hashSet){
+            System.out.println(member);
+        }
+        System.out.println();
+    }
 
 }
